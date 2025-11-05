d1e52781e49085c40fa41012fb0d0ef833e3d863
yeeeeni <yeeeeni@idstrust.com>
2025-11-05 14:19:36 +0900

    fix: retry필요한 메세지가 있을 경우와 없을 경우 분리

--- lib/src/internal/component/basic/sbu_placeholder_component.dart
+++ lib/src/internal/component/basic/sbu_placeholder_component.dart
@@ -25,62 +25,86 @@ class SBUPlaceholderComponent extends SBUStatelessComponent {
 
   @override
   Widget build(BuildContext context) {
-    final retryWidget = retryText != null
-        ? Padding(
-            padding: const EdgeInsets.only(top: 16),
-            child: Material(
-              color: Colors.transparent,
-              child: InkWell(
-                onTap: onRetryButtonClicked,
-                child: Container(
-                  width: 84,
-                  height: 32,
-                  padding: const EdgeInsets.only(
-                      left: 8, top: 4, right: 8, bottom: 4),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      SBUIconComponent(
-                        iconSize: 24,
-                        iconData: SBUIcons.refresh,
-                        iconColor: isLightTheme
-                            ? SBUColors.primaryMain
-                            : SBUColors.primaryLight,
-                      ),
-                      const SizedBox(height: 12),
-                      SBUTextComponent(
-                        text: retryText!,
-                        textType: SBUTextType.body3,
-                        textColorType: SBUTextColorType.primary,
-                      ),
-                    ],
-                  ),
-                ),
+    if (retryText == null) {
+      return Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+                'packages/sendbird_uikit/assets/images/image_no_data.png',
+                width: 120,
+                height: 120),
+            const SizedBox(height: 12),
+            Text(
+              '조회 된 메시지가 없어요',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: const Color(0xFF8F96A1),
+                fontSize: 16,
+                fontFamily: 'Pretendard',
+                fontWeight: FontWeight.w600,
+                height: 1.50,
+                letterSpacing: -0.32,
               ),
             ),
-          )
-        : null;
+          ],
+        ),
+      );
+    }
+
+    // retryText가 있는 경우 기존 로직
+    final retryWidget = Padding(
+      padding: const EdgeInsets.only(top: 16),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: onRetryButtonClicked,
+          child: Container(
+            width: 84,
+            height: 32,
+            padding:
+                const EdgeInsets.only(left: 8, top: 4, right: 8, bottom: 4),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SBUIconComponent(
+                  iconSize: 24,
+                  iconData: SBUIcons.refresh,
+                  iconColor: isLightTheme
+                      ? SBUColors.primaryMain
+                      : SBUColors.primaryLight,
+                ),
+                const SizedBox(height: 12),
+                SBUTextComponent(
+                  text: retryText!,
+                  textType: SBUTextType.body3,
+                  textColorType: SBUTextColorType.primary,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
 
     return Center(
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset('assets/images/image_no_data.png',
-              width: 120, height: 120),
-          const SizedBox(height: 12),
-          Text(
-            '조회 된 메시지가 없어요',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: const Color(0xFF8F96A1),
-              fontSize: 16,
-              fontFamily: 'Pretendard',
-              fontWeight: FontWeight.w600,
-              height: 1.50,
-              letterSpacing: -0.32,
-            ),
+          SBUIconComponent(
+            iconSize: 64,
+            iconData: iconData,
+            iconColor: isLightTheme
+                ? SBUColors.background700
+                : SBUColors.background100,
           ),
-          retryWidget ?? Container(),
+          const SizedBox(height: 24),
+          SBUTextComponent(
+            text: text,
+            textType: SBUTextType.body3,
+            textColorType: SBUTextColorType.text02,
+          ),
+          retryWidget,
         ],
       ),
     );
